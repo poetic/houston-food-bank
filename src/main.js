e4decb1f76122235ef13a85e821bb0a72bb62d39
Evan Corl <ecorl@apple.com>
2017-03-20 16:39:49 -0500

    Instantiate Vue empty cart modal component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import tracker from './components/tracker';
 import groceryListTabs from './components/grocery-list-tabs';
 import groceryListTabsContent from './components/grocery-list-tabs-content';
 import cart from './components/cart';
+import emptyCartModal from './components/empty-cart-modal';
 import instantiate from './modules/instantiate';
 
 instantiate([
@@ -9,4 +10,5 @@ instantiate([
   groceryListTabs,
   groceryListTabsContent,
   cart,
+  emptyCartModal,
 ]);
